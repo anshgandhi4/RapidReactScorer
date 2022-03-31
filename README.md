dceed223b0084e29623ae5061c0b6dd310fe6319
= <ansh.goodboy@gmail.com>
2022-03-30 18:07:17 -0700

    the real "Initial commit"

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RapidReactScorer
 A scorer app for the FRC Rapid React Season.
+
+Please share this app with others if you enjoyed it!
+
+This source code is licensed under the GNU General Purpose License. A copy of the license is provided in this project under the name `COPYING.txt`. Please visit https://www.gnu.org/licenses/ for license details.
---
+++ android/app/src/main/java/com/anshgandhi/rapid_react/MainActivity.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2022 Ansh Gandhi
+ *
+ * This file is part of Rapid React Scorer.
+ *
+ * Rapid React Scorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rapid React Scorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Rapid React Scorer.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Original Author: Ansh Gandhi
+ * Original Source Code: <https://github.com/anshgandhi4/RapidReactScorer>
+ *
+ * EVERYTHING ABOVE THIS LINE MUST BE KEPT AS IS UNDER GNU GPL LICENSE RULES.
+ */
+
+package com.anshgandhi.rapid_react;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
